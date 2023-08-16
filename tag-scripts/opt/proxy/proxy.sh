ebdd4b9c68d5713e3816d3d8770793420929fa0c
WouiTC <wouitc132@qq.com>
2023-08-16 17:18:54 +0800

    through shell scripts get clash ip

--- tag-scripts/opt/proxy/proxy.sh
+++ tag-scripts/opt/proxy/proxy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 host_ip=$(cat /etc/resolv.conf |grep "nameserver" |cut -f 2 -d " ") # 获取ip地址
 echo $host_ip # 输出ip地址
-export ALL_PROXY="http://$host_ip:7890" # 设置代理，7890为我的代理端口
+export http_proxy="http://$host_ip:7890"  
+export https_proxy="http://$host_ip:7890"
+export all_proxy="sock5://$host_ip:7890"
+export ALL_PROXY="sock5://$host_ip:7890"
 curl -I https://www.google.com # 尝试访问Google, 验证代理的有效性
